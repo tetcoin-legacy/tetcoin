4bd8867faac1ef8ee02298853b15569bdf75907c
Marlon Hanks <corporate@kedia.us>
2014-04-09 15:27:29 -0400

    Update checkpoints.cpp
    Updated CCheckpointData

--- tetcoind/src/checkpoints.cpp
+++ tetcoind/src/checkpoints.cpp
@@ -58,7 +58,7 @@ namespace Checkpoints
     static const CCheckpointData data = {
         &mapCheckpoints,
         1397069764, // * UNIX timestamp of last checkpoint block
-        2179203,    // * total number of transactions between genesis and last checkpoint
+        9068,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         8000.0     // * estimated number of transactions per day after checkpoint
     };
