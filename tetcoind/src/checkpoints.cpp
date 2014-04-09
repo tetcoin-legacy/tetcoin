8128afdf5da66780104d95c923e597d36229a620
Marlon Hanks <corporate@kedia.us>
2014-04-09 15:07:50 -0400

    Update checkpoints.cpp
    Added checkpoints

--- tetcoind/src/checkpoints.cpp
+++ tetcoind/src/checkpoints.cpp
@@ -35,28 +35,29 @@ namespace Checkpoints
     // + Contains no strange transactions
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        	(  0, uint256("0x40b2563f2a626df025db35810932334daa363f1d66e75e086486a04368afd62a"))
-		//(  3, uint256("0x0000023df93cdd3a8aad9ae252b549be7ee0087f50772db253cd410cbf3bcd8d"))
-		//(  8, uint256("0x00000a908100e13ce0394744fadbd09017a4272800565cd002ad883d35988f16"))
-		//( 68, uint256("0x00000de46a665e09312b10f798f374e631a7b757fc4d05f0c5612234591e3f2a"))
-        //(  1500, uint256("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967"))
-        //(  4032, uint256("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846"))
-        //(  8064, uint256("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70"))
-        //( 16128, uint256("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d"))
-        //( 23420, uint256("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507"))
-        //( 50000, uint256("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6"))
-        //( 80000, uint256("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a"))
-        //(120000, uint256("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131"))
-        //(161500, uint256("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43"))
-        //(179620, uint256("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709"))
-        //(240000, uint256("0x7140d1c4b4c2157ca217ee7636f24c9c73db39c4590c4e6eab2e3ea1555088aa"))
-        //(383640, uint256("0x2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364"))
-        //(409004, uint256("0x487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3"))
-        //(456000, uint256("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004"))
+        	//(    0, uint256("0x40b2563f2a626df025db35810932334daa363f1d66e75e086486a04368afd62a"))
+		(     1, uint256("0x20ba7c3117bdc6ed4f9ed9056604a22353aabdf9404a7110a162f3be939e2365"))
+		(  1000, uint256("0x9547cf9c731e3b28729f503a66cc876328eb8fa8c81f4269e7ee003e3c3fe7b3"))
+		(  2000, uint256("0x0e9a39fc44c6bb4771a5e0c4990049aec9596857392e1671f407bcb3efd13e90"))
+        	(  3000, uint256("0x5fd2927fcdbe2e07fac5588d07432e9fa1af8902afc960a66da870789d5a372d"))
+        	(  4000, uint256("0x0f0c26603474d02b9bf706cba198a7447589e0b10bbe5caa415e5f9f81ae71ab"))
+        	(  5000, uint256("0x39ee78b8d80f850f0603ac98c888f31cb287e9f9ddddcf2fe0dc04b2e44b4fc3"))
+        	(  6000, uint256("0xd1ae019feb27291270bcad2ef895e8e2a207e3fa187d20e1cf2de0e4457239d5"))
+        	(  7000, uint256("0xc4343e7858f07f9b16cae09418343e5491a3c394afde1bb83acd7c8222a81d0f"))
+        	(  8000, uint256("0x78449e84bf1521e3ee84159bb2e87c7ee0961613c56f0cde7ffa3dcc0d885653"))
+        	(  9000, uint256("0x9967659ad9151bb143fdc32e2ac85513e898a4e82c88efd0f0787098cb21604f"))
+        	(  9056, uint256("0x13d95ff35dac19627bbb6c6df36d785196c531bda635ce77f30c2ec60c4a3086"))
         ;
-    static const CCheckpointData data = {
+    /*static const CCheckpointData data = {
         &mapCheckpoints,
         1383887110, // * UNIX timestamp of last checkpoint block
+        2179203,    // * total number of transactions between genesis and last checkpoint
+                    //   (the tx=... number in the SetBestChain debug.log lines)
+        8000.0     // * estimated number of transactions per day after checkpoint
+    };*/
+    static const CCheckpointData data = {
+        &mapCheckpoints,
+        1397069764, // * UNIX timestamp of last checkpoint block
         2179203,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         8000.0     // * estimated number of transactions per day after checkpoint
