c8c28c90695ddc2954427c85199f30d7649d1057
Marlon Hanks <corporate@kedia.us>
2018-02-27 00:31:02 +0000

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 15, // Czarcoin addresses start with 7
+        PUBKEY_ADDRESS = 28, // Czarcoin addresses start with C
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
